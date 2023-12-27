5cec31ecce16d98b00a8b9daebcfde821e87ba42
Tinam Durge <tinamdurge02@gmail.com>
2023-12-27 11:48:26 +0530

    Added method to insert node

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Node {
+public class Node<T extends Comparable<T>> {
+    T data;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
